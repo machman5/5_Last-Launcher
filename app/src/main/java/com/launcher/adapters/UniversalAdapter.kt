c1ac8239b2ff65aebfd2f437562eabfe141b7c69
loitp <loitp@galaxy.one>
2023-01-29 13:55:47 +0700

    update

--- app/src/main/java/com/launcher/adapters/UniversalAdapter.kt
+++ app/src/main/java/com/launcher/adapters/UniversalAdapter.kt
@@ -1,72 +1,51 @@
-/*
- * Last Launcher
- * Copyright (C) 2019 Shubham Tyagi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+package com.launcher.adapters
 
-package com.launcher.adapters;
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.R
+import com.launcher.model.Apps
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-
-import com.R;
-import com.launcher.model.Apps;
-
-//
 // this adapter class is used for Frozen app list and Hidden app list
-//
-public class UniversalAdapter extends ArrayAdapter<Apps> {
+class UniversalAdapter(
+    mContext: Context, private val list: ArrayList<Apps>
+) : ArrayAdapter<Apps>(
+    mContext, R.layout.list_item, list
+) {
 
-    private final Context context;
-    private final ArrayList<Apps> list;
-    private OnClickListener listener;
+    private var listener: OnClickListener? = null
 
-    public UniversalAdapter(Context context, ArrayList<Apps> list) {
-        super(context, R.layout.list_item, list);
-        this.context = context;
-        this.list = list;
+    fun setOnClickListener(listener: OnClickListener?) {
+        this.listener = listener
     }
 
-    public void setOnClickListener(OnClickListener listener) {
-        this.listener = listener;
+    @SuppressLint("ViewHolder")
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val row = inflater.inflate(R.layout.list_item, parent, false)
+        row.tag = position
+        val tvAppLabel = row.findViewById<TextView>(R.id.app_label)
+        tvAppLabel.text = list[position].getAppName()
+        tvAppLabel.tag = position
+
+        row.setOnClickListener { view: View ->
+            listener?.onClick(
+                apps = getItem(view.tag as Int), view = view
+            )
+        }
+        tvAppLabel.setOnClickListener { view: View ->
+            listener?.onClick(
+                apps = getItem(view.tag as Int), view = view
+            )
+        }
+        return row
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View row = inflater.inflate(R.layout.list_item, parent, false);
-        row.setTag(position);
-        TextView textView = row.findViewById(R.id.app_label);
-        textView.setText(list.get(position).getAppName());
-        textView.setTag(position);
-
-        row.setOnClickListener(view -> listener.onClick(getItem((int) view.getTag()), view));
-
-        textView.setOnClickListener(view -> listener.onClick(getItem((int) view.getTag()), view));
-        return row;
+    interface OnClickListener {
+        fun onClick(apps: Apps?, view: View)
     }
-
-
-    public interface OnClickListener {
-        void onClick(Apps apps, View view);
-    }
-
 }

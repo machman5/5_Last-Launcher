111ed5aad43544283129360cdb80a2c01355a86a
loitp <loitp@galaxy.one>
2023-01-30 14:06:46 +0700

    update

--- app/src/main/java/com/launcher/dialogs/ColorSizeDialog.kt
+++ app/src/main/java/com/launcher/dialogs/ColorSizeDialog.kt
@@ -1,176 +1,156 @@
-/*
- * Last Launcher
- * Copyright (C) 2019 Shubham Tyagi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+package com.launcher.dialogs
 
-package com.launcher.dialogs;
-
-import android.app.Dialog;
-import android.content.Context;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.Window;
-import android.widget.TextView;
-
-import com.R;
-import com.launcher.utils.DbUtils;
-import com.launcher.views.colorseekbar.ColorSeekBar;
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.widget.TextView
+import com.R
+import com.launcher.utils.DbUtils
+import com.launcher.utils.DbUtils.maxAppSize
+import com.launcher.utils.DbUtils.minAppSize
+import com.launcher.utils.DbUtils.putAppColor
+import com.launcher.utils.DbUtils.putAppSize
+import com.launcher.views.colorseekbar.ColorSeekBar
+import com.launcher.views.colorseekbar.ColorSeekBar.OnColorChangeListener
 
 // choose color Dialog
-public class ColorSizeDialog extends Dialog {
+class ColorSizeDialog     // boolean change=false;
+    (
+    context: Context,
+    private val appPackage: String,
+    private var appColor: Int,
+    private val textView: TextView,
+    private var appSize: Int
+) : Dialog(
+    context
+) {
+    private val handler = Handler(Looper.getMainLooper())
+    private var runnable: Runnable? = null
 
-    private static final int DELAY = 25;
-    //private static final String TAG = "ChooseSize";
-    private final static int DEFAULT_MIN_TEXT_SIZE = DbUtils.getMinAppSize();
-    private final static int DEFAULT_MAX_TEXT_SIZE = DbUtils.getMaxAppSize();
+    override fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
 
-    private final Handler handler = new Handler();
-    private final String appPackage;
-    private final TextView textView;
-
-    private int appSize;
-    private Runnable runnable;
-    private int appColor;
-    // boolean change=false;
-
-    public ColorSizeDialog(Context context, String appPackage, int appColor, TextView textView, int appSize) {
-        super(context);
-        this.appPackage = appPackage;
-        this.appColor = appColor;
-        this.textView = textView;
-        this.appSize = appSize;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         // getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
         // no title please
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.dialog_color_size);
-        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_color_size)
+        window?.setBackgroundDrawableResource(android.R.color.transparent)
 
-
-        ColorSeekBar colorSeekBar = findViewById(R.id.colorSlider1);
-        colorSeekBar.setMaxPosition(100);
+        val colorSlider1 = findViewById<ColorSeekBar>(R.id.colorSlider1)
+        colorSlider1.setMaxPosition(100)
         //colorSeekBar.setColorSeeds(R.array.material_colors);
-        colorSeekBar.setShowAlphaBar(true);
-        colorSeekBar.setBarHeight(8);
+        colorSlider1.isShowAlphaBar = true
+        colorSlider1.setBarHeight(8f)
 
 
         // todo: is this still correct?
-        if (appColor != DbUtils.NULL_TEXT_COLOR)
-            colorSeekBar.setColor(appColor);
+        if (appColor != DbUtils.NULL_TEXT_COLOR) {
+            colorSlider1.color = appColor
+        }
         // set the color and save this to database
-        colorSeekBar.setOnColorChangeListener((colorBarPosition, alphaBarPosition, color) -> {
-            textView.setTextColor(color);
-            appColor = color;
-            // change=true;
-            // DbUtils.putAppColorImmediately(appPackage, color);
-        });
+        colorSlider1.setOnColorChangeListener(object : OnColorChangeListener {
+            override fun onColorChangeListener(
+                colorBarPosition: Int, alphaBarPosition: Int, color: Int
+            ) {
+                textView.setTextColor(color)
+                appColor = color
+            }
+        })
 
 
         // size related
-        TextView plus = findViewById(R.id.btn_plus);
-        TextView minus = findViewById(R.id.btn_minus);
-        TextView size = findViewById(R.id.tv_size);
-
-        size.setText(String.valueOf(appSize));
-
-
-        plus.setOnClickListener(view -> {
+        val btnPlus = findViewById<TextView>(R.id.btn_plus)
+        val btnMinus = findViewById<TextView>(R.id.btn_minus)
+        val tvSize = findViewById<TextView>(R.id.tv_size)
+        tvSize.text = appSize.toString()
+        btnPlus.setOnClickListener {
 
             // change=true;
-            appSize++;
-
+            appSize++
             if (appSize >= DEFAULT_MAX_TEXT_SIZE) {
-                appSize = DEFAULT_MAX_TEXT_SIZE;
+                appSize = DEFAULT_MAX_TEXT_SIZE
                 //   plus.setClickable(false);
             }
-
-            size.setText(String.valueOf(appSize));
-            textView.setTextSize(appSize);
-        });
-
-        minus.setOnClickListener(view -> {
+            tvSize.text = appSize.toString()
+            textView.textSize = appSize.toFloat()
+        }
+        btnMinus.setOnClickListener {
             //change=true;
-            --appSize;
+            --appSize
             if (appSize < DEFAULT_MIN_TEXT_SIZE) {
-                appSize = DEFAULT_MIN_TEXT_SIZE;
+                appSize = DEFAULT_MIN_TEXT_SIZE
             }
-
-            size.setText(String.valueOf(appSize));
-            textView.setTextSize(appSize);
-        });
-
-
-        plus.setOnLongClickListener(view -> {
-            runnable = () -> {
-                if (!plus.isPressed()) {
-                    handler.removeCallbacks(runnable);
-                    return;
+            tvSize.text = appSize.toString()
+            textView.textSize = appSize.toFloat()
+        }
+        btnPlus.setOnLongClickListener {
+            runnable = Runnable {
+                if (!btnPlus.isPressed) {
+                    runnable?.apply {
+                        handler.removeCallbacks(this)
+                    }
+                    return@Runnable
                 }
                 // increase value
                 // change=true;
-                appSize++;
-
+                appSize++
                 if (appSize >= DEFAULT_MAX_TEXT_SIZE) {
-                    appSize = DEFAULT_MAX_TEXT_SIZE;
+                    appSize = DEFAULT_MAX_TEXT_SIZE
                     //   plus.setClickable(false);
                 }
-
-                size.setText(String.valueOf(appSize));
-                textView.setTextSize(appSize);
-                handler.postDelayed(runnable, DELAY);
-            };
-            handler.removeCallbacks(runnable);
-            handler.postDelayed(runnable, DELAY);
-            return true;
-        });
-
-
-        minus.setOnLongClickListener(view -> {
-            runnable = () -> {
-                if (!minus.isPressed()) {
-                    handler.removeCallbacks(runnable);
-                    return;
+                tvSize.text = appSize.toString()
+                textView.textSize = appSize.toFloat()
+                runnable?.apply {
+                    handler.postDelayed(this, DELAY.toLong())
+                }
+            }
+            runnable?.apply {
+                handler.removeCallbacks(this)
+                handler.postDelayed(this, DELAY.toLong())
+            }
+            true
+        }
+        btnMinus.setOnLongClickListener {
+            runnable = Runnable {
+                if (!btnMinus.isPressed) {
+                    runnable?.apply {
+                        handler.removeCallbacks(this)
+                    }
+                    return@Runnable
                 }
                 // decrease value
-                --appSize;
+                --appSize
                 // change=true;
                 if (appSize < DEFAULT_MIN_TEXT_SIZE) {
-                    appSize = DEFAULT_MIN_TEXT_SIZE;
+                    appSize = DEFAULT_MIN_TEXT_SIZE
                 }
-
-                size.setText(String.valueOf(appSize));
-                textView.setTextSize(appSize);
-                handler.postDelayed(runnable, DELAY);
-            };
-            handler.removeCallbacks(runnable);
-            handler.postDelayed(runnable, DELAY);
-            return true;
-        });
-
+                tvSize.text = appSize.toString()
+                textView.textSize = appSize.toFloat()
+                runnable?.apply {
+                    handler.postDelayed(this, DELAY.toLong())
+                }
+            }
+            runnable?.apply {
+                handler.removeCallbacks(this)
+                handler.postDelayed(this, DELAY.toLong())
+            }
+            true
+        }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        DbUtils.putAppColor(appPackage, appColor);
-        DbUtils.putAppSize(appPackage, appSize);
+    override fun onStop() {
+        super.onStop()
+        putAppColor(activityName = appPackage, color = appColor)
+        putAppSize(activityName = appPackage, size = appSize)
+    }
 
+    companion object {
+        private const val DELAY = 25
+
+        private val DEFAULT_MIN_TEXT_SIZE = minAppSize
+        private val DEFAULT_MAX_TEXT_SIZE = maxAppSize
     }
 }

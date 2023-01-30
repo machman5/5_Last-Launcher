ffc6e894b453b2e81a50d9d1e8cc8be62ad1a0a5
loitp <loitp@galaxy.one>
2023-01-30 14:36:07 +0700

    update

--- app/src/main/java/com/launcher/dialogs/HiddenAppsDialogs.kt
+++ app/src/main/java/com/launcher/dialogs/HiddenAppsDialogs.kt
@@ -1,114 +1,101 @@
-/*
- * Last Launcher
- * Copyright (C) 2019,2020 Shubham Tyagi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+package com.launcher.dialogs
 
-package com.launcher.dialogs;
+import android.app.Dialog
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.MenuItem
+import android.view.View
+import android.view.Window
+import android.widget.ListView
+import android.widget.PopupMenu
+import com.R
+import com.launcher.adapters.UniversalAdapter
+import com.launcher.model.Apps
+import com.launcher.utils.DbUtils.theme
+import java.util.*
 
-import android.app.Dialog;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.ContextThemeWrapper;
-import android.view.View;
-import android.view.Window;
-import android.widget.ListView;
-import android.widget.PopupMenu;
+class HiddenAppsDialogs(
+    mContext: Context,
+    private val mAppsList: List<Apps>
+) : Dialog(
+    mContext
+) {
+    private var hiddenApps = ArrayList<Apps>()
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+    override fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
 
-import com.R;
-import com.launcher.adapters.UniversalAdapter;
-import com.launcher.model.Apps;
-import com.launcher.utils.DbUtils;
-
-public class HiddenAppsDialogs extends Dialog {
-
-    private final List<Apps> mAppsList;
-    private final Context context;
-    ArrayList<Apps> hiddenApps = new ArrayList<>();
-
-    public HiddenAppsDialogs(Context context, List<Apps> appsList) {
-        super(context);
-        this.context = context;
-        mAppsList = appsList;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.dialog_hidden_apps);
-        ListView listView = findViewById(R.id.hidden_app_list);
-
-        UniversalAdapter adapter = new UniversalAdapter(context, hiddenApps);
-        listView.setAdapter(adapter);
-        adapter.setOnClickListener(this::confirmationAndRemove);
-
-    }
-
-
-    private void confirmationAndRemove(Apps apps, View view) {
-
-        Context ctx;
-        if (DbUtils.getTheme() == R.style.Wallpaper)
-            ctx = new ContextThemeWrapper(context, R.style.AppTheme);
-        else
-            ctx = new ContextThemeWrapper(context, DbUtils.getTheme());
-
-        PopupMenu popupMenu = new PopupMenu(ctx, view);
-        popupMenu.getMenuInflater().inflate(R.menu.remove_popup, popupMenu.getMenu());
-
-        popupMenu.setOnMenuItemClickListener(menuItem -> {
-
-            if (menuItem.getItemId() == R.id.menu_remove_this) {
-                apps.setAppHidden(false);
-                updateHiddenList();
-            } else if (menuItem.getItemId() == R.id.menu_run_this_app) {
-                if (!apps.isShortcut()) {
-                    String[] strings = apps.getActivityName().split("/");
-                    final Intent intent = new Intent(Intent.ACTION_MAIN, null);
-                    intent.setClassName(strings[0], strings[1]);
-                    intent.setComponent(new ComponentName(strings[0], strings[1]));
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    context.startActivity(intent);
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_hidden_apps)
+        val listView = findViewById<ListView>(R.id.hidden_app_list)
+        val adapter = UniversalAdapter(context, hiddenApps)
+        listView.adapter = adapter
+        adapter.setOnClickListener(object : UniversalAdapter.OnClickListener {
+            override fun onClick(apps: Apps?, view: View) {
+                apps?.apply {
+                    confirmationAndRemove(
+                        apps = this,
+                        view = view
+                    )
                 }
-
             }
-            return true;
 
-        });
-        popupMenu.show();
-
+        })
     }
 
+    private fun confirmationAndRemove(
+        apps: Apps,
+        view: View
+    ) {
+        val ctx: Context = if (theme == R.style.Wallpaper) {
+            ContextThemeWrapper(
+                /* base = */ context,
+                /* themeResId = */ R.style.AppTheme
+            )
+        } else {
+            ContextThemeWrapper(
+                /* base = */ context,
+                /* themeResId = */ theme
+            )
+        }
+        val popupMenu = PopupMenu(/* context = */ ctx, /* anchor = */ view)
+        popupMenu.menuInflater.inflate(R.menu.remove_popup, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            if (menuItem.itemId == R.id.menu_remove_this) {
+                apps.setAppHidden(false)
+                updateHiddenList()
+            } else if (menuItem.itemId == R.id.menu_run_this_app) {
+                if (!apps.isShortcut) {
+                    apps.activityName?.let { name ->
+                        val strings = name.split("/".toRegex()).dropLastWhile { it.isEmpty() }
+                            .toTypedArray()
+                        val intent = Intent(Intent.ACTION_MAIN, null)
+                        intent.setClassName(strings[0], strings[1])
+                        intent.component = ComponentName(strings[0], strings[1])
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        context.startActivity(intent)
+                    }
+                }
+            }
+            true
+        }
+        popupMenu.show()
+    }
 
-    public int updateHiddenList() {
-        synchronized (mAppsList) {
-            for (Apps apps : mAppsList) {
-                if (apps.isHidden()) {
-                    hiddenApps.add(apps);
+    fun updateHiddenList(): Int {
+        synchronized(mAppsList) {
+            for (apps in mAppsList) {
+                if (apps.isHidden) {
+                    hiddenApps.add(apps)
                 }
             }
         }
-        Collections.sort(hiddenApps, (o1, o2) -> o1.getAppName().compareToIgnoreCase(o2.getAppName()));
-        return hiddenApps.size();
+        hiddenApps.sortWith { o1: Apps, o2: Apps ->
+            o1.getAppName().compareTo(o2.getAppName(), ignoreCase = true)
+        }
+        return hiddenApps.size
     }
-
 }

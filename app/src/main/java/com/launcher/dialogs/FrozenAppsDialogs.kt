545c6f1d4cf3c7af39b20ff39167bfaeaf17680b
loitp <loitp@galaxy.one>
2023-01-30 14:15:56 +0700

    update

--- app/src/main/java/com/launcher/dialogs/FrozenAppsDialogs.kt
+++ app/src/main/java/com/launcher/dialogs/FrozenAppsDialogs.kt
@@ -1,100 +1,79 @@
-/*
- * Last Launcher
- * Copyright (C) 2019,2020 Shubham Tyagi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+package com.launcher.dialogs
 
-package com.launcher.dialogs;
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.view.ContextThemeWrapper
+import android.view.MenuItem
+import android.view.View
+import android.view.Window
+import android.widget.ListView
+import android.widget.PopupMenu
+import com.R
+import com.launcher.adapters.UniversalAdapter
+import com.launcher.model.Apps
+import com.launcher.utils.DbUtils.theme
 
-import android.app.Dialog;
-import android.content.Context;
-import android.os.Bundle;
-import android.view.ContextThemeWrapper;
-import android.view.View;
-import android.view.Window;
-import android.widget.ListView;
-import android.widget.PopupMenu;
+class FrozenAppsDialogs(
+    mContext: Context,
+    private val mAppsList: List<Apps>
+) : Dialog(
+    mContext
+) {
+    private var frozenApps = ArrayList<Apps>()
 
-import java.util.ArrayList;
-import java.util.List;
+    override fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
 
-import com.R;
-import com.launcher.adapters.UniversalAdapter;
-import com.launcher.model.Apps;
-import com.launcher.utils.DbUtils;
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_hidden_apps)
+        val listView = findViewById<ListView>(R.id.hidden_app_list)
+        val adapter = UniversalAdapter(context, frozenApps)
+        listView.adapter = adapter
 
-public class FrozenAppsDialogs extends Dialog {
-
-    private final List<Apps> mAppsList;
-    private final Context context;
-    ArrayList<Apps> frozenApps = new ArrayList<>();
-    private ListView listView;
-
-    public FrozenAppsDialogs(Context context, List<Apps> appsList) {
-        super(context);
-        this.context = context;
-        mAppsList = appsList;
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.dialog_hidden_apps);
-        listView = findViewById(R.id.hidden_app_list);
-
-        UniversalAdapter adapter = new UniversalAdapter(context, frozenApps);
-        listView.setAdapter(adapter);
-
-        adapter.setOnClickListener(this::confirmationAndRemove);
-
-    }
-
-    private void confirmationAndRemove(Apps apps, View view) {
-        Context ctx;
-        if (DbUtils.getTheme() == R.style.Wallpaper)
-            ctx = new ContextThemeWrapper(context, R.style.AppTheme);
-        else
-            ctx = new ContextThemeWrapper(context, DbUtils.getTheme());
-
-        PopupMenu popupMenu = new PopupMenu(ctx, view);
-        popupMenu.getMenuInflater().inflate(R.menu.remove_popup, popupMenu.getMenu());
-
-        popupMenu.setOnMenuItemClickListener(menuItem -> {
-
-            if (menuItem.getItemId() == R.id.menu_remove_this) {
-                apps.setFreeze(false);
-                updateFrozenList();
+        adapter.setOnClickListener(object : UniversalAdapter.OnClickListener {
+            override fun onClick(apps: Apps?, view: View) {
+                apps?.let { a ->
+                    confirmationAndRemove(
+                        a,
+                        view
+                    )
+                }
             }
-            return true;
-
-        });
-        popupMenu.show();
-
+        })
     }
 
-    public int updateFrozenList() {
-        synchronized (mAppsList) {
+    private fun confirmationAndRemove(
+        apps: Apps,
+        view: View
+    ) {
+        val ctx: Context = if (theme == R.style.Wallpaper) ContextThemeWrapper(
+            context,
+            R.style.AppTheme
+        ) else ContextThemeWrapper(
+            context, theme
+        )
+        val popupMenu = PopupMenu(ctx, view)
+        popupMenu.menuInflater.inflate(R.menu.remove_popup, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { menuItem: MenuItem ->
+            if (menuItem.itemId == R.id.menu_remove_this) {
+                apps.setFreeze(false)
+                updateFrozenList()
+            }
+            true
+        }
+        popupMenu.show()
+    }
+
+    fun updateFrozenList(): Int {
+        synchronized(mAppsList) {
             // only show frozen app
-            for (Apps apps : mAppsList) {
-                if (apps.isSizeFrozen()) {
-                    frozenApps.add(apps);
+            for (apps in mAppsList) {
+                if (apps.isSizeFrozen) {
+                    frozenApps.add(apps)
                 }
             }
         }
-        return frozenApps.size();
+        return frozenApps.size
     }
-
 }

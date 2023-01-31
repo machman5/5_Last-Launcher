aef6c4b0460af79e06b8f9ed84bfa2d0a1390aa8
loitp <loitp@galaxy.one>
2023-01-31 17:21:35 +0700

    update

--- app/src/main/java/com/launcher/dialogs/launcher/ThemeSelectorDialog.kt
+++ app/src/main/java/com/launcher/dialogs/launcher/ThemeSelectorDialog.kt
@@ -4,8 +4,8 @@ import android.app.Dialog
 import android.os.Bundle
 import android.view.View
 import android.view.Window
-import android.widget.LinearLayout
 import com.R
+import com.databinding.DlgThemeSelectorBinding
 import com.launcher.LauncherActivity
 import com.launcher.utils.DbUtils.externalSourceColor
 import com.launcher.utils.DbUtils.theme
@@ -14,27 +14,31 @@ class ThemeSelectorDialog internal constructor(
     private val launcherActivity: LauncherActivity
 ) : Dialog(launcherActivity), View.OnClickListener {
 
+    private lateinit var binding: DlgThemeSelectorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.dlg_theme_selector)
-        val ll = findViewById<LinearLayout>(R.id.theme_linear_layout)
-        for (i in 0 until ll.childCount) {
-            ll.getChildAt(i).setOnClickListener(this)
+
+        binding = DlgThemeSelectorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        for (i in 0 until binding.themeLinearLayout.childCount) {
+            binding.themeLinearLayout.getChildAt(i).setOnClickListener(this)
         }
     }
 
     override fun onClick(view: View) {
-        when (view.id) {
-            R.id.t1 -> setTheme(R.style.AppTheme)
-            R.id.t2 -> setTheme(R.style.Wallpaper)
-            R.id.t3 -> setTheme(R.style.Black)
-            R.id.t4 -> setTheme(R.style.White)
-            R.id.t5 -> setTheme(R.style.WhiteOnGrey)
-            R.id.t6 -> setTheme(R.style.BlackOnGrey)
-            R.id.t35 -> setTheme(R.style.Hacker_green)
-            R.id.t36 -> setTheme(R.style.Hacker_red)
+        when (view) {
+            binding.t1 -> setTheme(R.style.AppTheme)
+            binding.t2 -> setTheme(R.style.Wallpaper)
+            binding.t3 -> setTheme(R.style.Black)
+            binding.t4 -> setTheme(R.style.White)
+            binding.t5 -> setTheme(R.style.WhiteOnGrey)
+            binding.t6 -> setTheme(R.style.BlackOnGrey)
+            binding.t35 -> setTheme(R.style.Hacker_green)
+            binding.t36 -> setTheme(R.style.Hacker_red)
         }
     }
 

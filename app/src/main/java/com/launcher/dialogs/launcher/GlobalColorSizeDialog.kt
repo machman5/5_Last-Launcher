1a55f81290d16cd7ba37819f3c1a5cdfe41e747b
loitp <loitp@galaxy.one>
2023-01-31 17:25:36 +0700

    update

--- app/src/main/java/com/launcher/dialogs/launcher/GlobalColorSizeDialog.kt
+++ app/src/main/java/com/launcher/dialogs/launcher/GlobalColorSizeDialog.kt
@@ -6,8 +6,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.Window
-import android.widget.TextView
-import com.R
+import com.databinding.DlgColorSizeBinding
 import com.launcher.model.Apps
 import com.launcher.utils.Constants.DEFAULT_MAX_TEXT_SIZE
 import com.launcher.utils.Constants.DEFAULT_MIN_TEXT_SIZE
@@ -19,12 +18,10 @@ import com.launcher.utils.DbUtils.getAppColor
 import com.launcher.utils.DbUtils.getAppSize
 import com.launcher.utils.DbUtils.globalSizeAdditionExtra
 import com.launcher.utils.Utils.Companion.oftenAppsList
-import com.launcher.views.colorSeekBar.ColorSeekBar
 import com.launcher.views.colorSeekBar.ColorSeekBar.OnColorChangeListener
 
 class GlobalColorSizeDialog(
-    context: Context,
-    private val mAppsList: List<Apps>
+    context: Context, private val mAppsList: List<Apps>
 ) : Dialog(
     context
 ) {
@@ -39,25 +36,27 @@ class GlobalColorSizeDialog(
     private var appSize = 0
     private var mColor = 0
 
+    private lateinit var binding: DlgColorSizeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.dlg_color_size)
+
+        binding = DlgColorSizeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         window?.setBackgroundDrawableResource(android.R.color.transparent)
-        val colorSlider1 = findViewById<ColorSeekBar>(R.id.colorSlider1)
-        colorSlider1.setMaxPosition(100)
-        colorSlider1.isShowAlphaBar = true
-        colorSlider1.setBarHeight(8f)
+        binding.colorSlider1.setMaxPosition(100)
+        binding.colorSlider1.isShowAlphaBar = true
+        binding.colorSlider1.setBarHeight(8f)
         val colorDefault = appsColorDefault
         if (colorDefault != DbUtils.NULL_TEXT_COLOR) {
-            colorSlider1.color = colorDefault
-            //} else {
-            //do something
+            binding.colorSlider1.color = colorDefault
         }
 
         // set the color and save this to database
-        colorSlider1.setOnColorChangeListener(object : OnColorChangeListener {
+        binding.colorSlider1.setOnColorChangeListener(object : OnColorChangeListener {
             override fun onColorChangeListener(
                 colorBarPosition: Int, alphaBarPosition: Int, color: Int
             ) {
@@ -79,14 +78,9 @@ class GlobalColorSizeDialog(
 
         })
 
-        // size related
-        val btnPlus = findViewById<TextView>(R.id.btnPlus)
-        val btnMinus = findViewById<TextView>(R.id.btnMinus)
-        val tvSize = findViewById<TextView>(R.id.tvSize)
-
         appSize = globalSizeAdditionExtra
-        tvSize.text = appSize.toString()
-        btnPlus.setOnClickListener {
+        binding.tvSize.text = appSize.toString()
+        binding.btnPlus.setOnClickListener {
             appSize++
             if (appSize >= DEFAULT_MAX_TEXT_SIZE) {
                 appSize = DEFAULT_MAX_TEXT_SIZE
@@ -99,9 +93,8 @@ class GlobalColorSizeDialog(
                             // check if text size is null then set the size to default size
                             // size is null(-1) when user installed this app
                             if (textSize == DbUtils.NULL_TEXT_SIZE) {
-                                textSize = if (oftenApps.contains(
-                                        name.split("/".toRegex()).dropLastWhile { it.isEmpty() }
-                                            .toTypedArray()[0]
+                                textSize = if (oftenApps.contains(name.split("/".toRegex())
+                                        .dropLastWhile { it.isEmpty() }.toTypedArray()[0]
                                     )
                                 ) {
                                     DEFAULT_TEXT_SIZE_OFTEN_APPS
@@ -114,9 +107,9 @@ class GlobalColorSizeDialog(
                     }
                 }
             }
-            tvSize.text = appSize.toString()
+            binding.tvSize.text = appSize.toString()
         }
-        btnMinus.setOnClickListener {
+        binding.btnMinus.setOnClickListener {
             --appSize
             if (appSize < DEFAULT_MIN_TEXT_SIZE) {
                 appSize = DEFAULT_MIN_TEXT_SIZE
@@ -128,9 +121,8 @@ class GlobalColorSizeDialog(
                             // check if text size is null then set the size to default size
                             // size is null(-1) when user installed this app
                             if (textSize == DbUtils.NULL_TEXT_SIZE) {
-                                textSize = if (oftenApps.contains(
-                                        name.split("/".toRegex()).dropLastWhile { it.isEmpty() }
-                                            .toTypedArray()[0]
+                                textSize = if (oftenApps.contains(name.split("/".toRegex())
+                                        .dropLastWhile { it.isEmpty() }.toTypedArray()[0]
                                     )
                                 ) {
                                     DEFAULT_TEXT_SIZE_OFTEN_APPS
@@ -145,11 +137,11 @@ class GlobalColorSizeDialog(
                     }
                 }
             }
-            tvSize.text = appSize.toString()
+            binding.tvSize.text = appSize.toString()
         }
-        btnPlus.setOnLongClickListener {
+        binding.btnPlus.setOnLongClickListener {
             runnable = Runnable {
-                if (!btnPlus.isPressed) {
+                if (!binding.btnPlus.isPressed) {
                     runnable?.apply {
                         handler.removeCallbacks(this)
                     }
@@ -168,9 +160,8 @@ class GlobalColorSizeDialog(
                                 // check if text size is null then set the size to default size
                                 // size is null(-1) when user installed this app
                                 if (textSize == DbUtils.NULL_TEXT_SIZE) {
-                                    textSize = if (oftenApps.contains(
-                                            name.split("/".toRegex()).dropLastWhile { it.isEmpty() }
-                                                .toTypedArray()[0]
+                                    textSize = if (oftenApps.contains(name.split("/".toRegex())
+                                            .dropLastWhile { it.isEmpty() }.toTypedArray()[0]
                                         )
                                     ) {
                                         DEFAULT_TEXT_SIZE_OFTEN_APPS
@@ -185,7 +176,7 @@ class GlobalColorSizeDialog(
                         }
                     }
                 }
-                tvSize.text = appSize.toString()
+                binding.tvSize.text = appSize.toString()
                 runnable?.apply {
                     handler.postDelayed(this, DELAY)
                 }
@@ -196,9 +187,9 @@ class GlobalColorSizeDialog(
             }
             true
         }
-        btnMinus.setOnLongClickListener {
+        binding.btnMinus.setOnLongClickListener {
             runnable = Runnable {
-                if (!btnMinus.isPressed) {
+                if (!binding.btnMinus.isPressed) {
                     runnable?.apply {
                         handler.removeCallbacks(this)
                     }
@@ -216,9 +207,8 @@ class GlobalColorSizeDialog(
                                 // check if text size is null then set the size to default size
                                 // size is null(-1) when user installed this app
                                 if (textSize == DbUtils.NULL_TEXT_SIZE) {
-                                    textSize = if (oftenApps.contains(
-                                            name.split("/".toRegex()).dropLastWhile { it.isEmpty() }
-                                                .toTypedArray()[0]
+                                    textSize = if (oftenApps.contains(name.split("/".toRegex())
+                                            .dropLastWhile { it.isEmpty() }.toTypedArray()[0]
                                         )
                                     ) {
                                         DEFAULT_TEXT_SIZE_OFTEN_APPS
@@ -233,7 +223,7 @@ class GlobalColorSizeDialog(
                         }
                     }
                 }
-                tvSize.text = appSize.toString()
+                binding.tvSize.text = appSize.toString()
                 runnable?.apply {
                     handler.postDelayed(this, DELAY)
                 }

b5877f6c02a2d8552ec1b5d47426c58f4c479411
loitp <loitp@galaxy.one>
2023-02-01 14:58:02 +0700

    update

--- app/src/main/java/com/launcher/dialogs/launcher/frozen/FrozenAppsDialogs.kt
+++ app/src/main/java/com/launcher/dialogs/launcher/frozen/FrozenAppsDialogs.kt
@@ -10,6 +10,7 @@ import android.view.Window
 import android.widget.ListView
 import android.widget.PopupMenu
 import com.R
+import com.databinding.DlgHiddenAppsBinding
 import com.launcher.adapters.UniversalAdapter
 import com.launcher.model.Apps
 import com.launcher.utils.DbUtils.theme
@@ -20,23 +21,27 @@ class FrozenAppsDialogs(
 ) : Dialog(
     mContext
 ) {
+    private lateinit var binding: DlgHiddenAppsBinding
     private var frozenApps = ArrayList<Apps>()
+    private var adapter: UniversalAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         requestWindowFeature(Window.FEATURE_NO_TITLE)
-        setContentView(R.layout.dlg_hidden_apps)
-        val listView = findViewById<ListView>(R.id.lvHiddenApp)
-        val adapter = UniversalAdapter(context, frozenApps)
-        listView.adapter = adapter
 
-        adapter.setOnClickListener(object : UniversalAdapter.OnClickListener {
+        binding = DlgHiddenAppsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = UniversalAdapter(context, frozenApps)
+        binding.lvHiddenApp.adapter = adapter
+
+        adapter?.setOnClickListener(object : UniversalAdapter.OnClickListener {
             override fun onClick(apps: Apps?, view: View) {
                 apps?.let { a ->
                     confirmationAndRemove(
-                        a,
-                        view
+                        apps = a,
+                        view = view
                     )
                 }
             }
@@ -59,6 +64,10 @@ class FrozenAppsDialogs(
             if (menuItem.itemId == R.id.menuRemoveThis) {
                 apps.setFreeze(false)
                 updateFrozenList()
+                adapter?.notifyDataSetChanged()
+                if (frozenApps.isEmpty()) {
+                    dismiss()
+                }
             }
             true
         }
@@ -68,6 +77,7 @@ class FrozenAppsDialogs(
     fun updateFrozenList(): Int {
         synchronized(mAppsList) {
             // only show frozen app
+            frozenApps.clear()
             for (apps in mAppsList) {
                 if (apps.isSizeFrozen) {
                     frozenApps.add(apps)

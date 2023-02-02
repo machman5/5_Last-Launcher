e8ebb27d1e278ae0bb18c005ff89333cac83ae1f
loitp <loitp@galaxy.one>
2023-02-02 14:40:00 +0700

    update

--- app/src/main/java/com/launcher/dialogs/launcher/sort/SortDialog.kt
+++ app/src/main/java/com/launcher/dialogs/launcher/sort/SortDialog.kt
@@ -2,24 +2,20 @@ package com.launcher.dialogs.launcher.sort
 
 import android.app.Dialog
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.view.Window
 import com.R
-import com.databinding.DlgAppFontBinding
 import com.databinding.DlgAppSortBinding
 import com.launcher.LauncherActivity
+import com.launcher.ext.click
 import com.launcher.utils.Constants
-import com.launcher.utils.DbUtils
 
 class SortDialog(
-    mContext: Context,
-    private val launcherActivity: LauncherActivity
+    mContext: Context, private val launcherActivity: LauncherActivity
 ) : Dialog(
     mContext,
     R.style.DialogSlideUpAnim,
-), View.OnClickListener {
+) {
     private lateinit var binding: DlgAppSortBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,36 +26,30 @@ class SortDialog(
         binding = DlgAppSortBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.menuSortByName.setOnClickListener(this)
-        binding.menuSortByOpeningCounts.setOnClickListener(this)
-        binding.menuSortByColor.setOnClickListener(this)
-        binding.menuSortBySize.setOnClickListener(this)
-        binding.menuSortByUpdateTime.setOnClickListener(this)
-        binding.menuSortByRecentUse.setOnClickListener(this)
-    }
-
-    override fun onClick(v: View) {
-        when (v) {
-            binding.menuSortByName -> {
-                launcherActivity.sortApps(Constants.SORT_BY_NAME)
-            }
-            binding.menuSortByOpeningCounts -> {
-                launcherActivity.sortApps(Constants.SORT_BY_OPENING_COUNTS)
-            }
-            binding.menuSortByColor -> {
-                launcherActivity.sortApps(Constants.SORT_BY_COLOR)
-            }
-            binding.menuSortBySize -> {
-                launcherActivity.sortApps(Constants.SORT_BY_SIZE)
-            }
-            binding.menuSortByUpdateTime -> {
-                launcherActivity.sortApps(Constants.SORT_BY_UPDATE_TIME)
-            }
-            binding.menuSortByRecentUse -> {
-                launcherActivity.sortApps(Constants.SORT_BY_RECENT_OPEN)
-            }
+        binding.menuSortByName.click {
+            launcherActivity.sortApps(Constants.SORT_BY_NAME)
+            dismiss()
         }
-        dismiss()
+        binding.menuSortByOpeningCounts.click {
+            launcherActivity.sortApps(Constants.SORT_BY_OPENING_COUNTS)
+            dismiss()
+        }
+        binding.menuSortByColor.click {
+            launcherActivity.sortApps(Constants.SORT_BY_COLOR)
+            dismiss()
+        }
+        binding.menuSortBySize.click {
+            launcherActivity.sortApps(Constants.SORT_BY_SIZE)
+            dismiss()
+        }
+        binding.menuSortByUpdateTime.click {
+            launcherActivity.sortApps(Constants.SORT_BY_UPDATE_TIME)
+            dismiss()
+        }
+        binding.menuSortByRecentUse.click {
+            launcherActivity.sortApps(Constants.SORT_BY_RECENT_OPEN)
+            dismiss()
+        }
     }
 
 }
